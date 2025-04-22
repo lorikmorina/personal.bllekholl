2bf4f53a4137bd73ea4871a907c96e30312fdee5
lorikmorina <lorik.morina1@student.uni-pr.edu>
2025-04-22 16:42:38 +0200

    #hotjar-tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,20 @@ export default function RootLayout({
           src="https://challenges.cloudflare.com/turnstile/v0/api.js"
           strategy="afterInteractive"
         />
+{/* Hotjar Tracking Code */}
+<Script id="hotjar-tracking" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6379589,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+
         <meta name="apple-mobile-web-app-title" content="SecureVibing" />
         
         {/* Favicon tags */}
