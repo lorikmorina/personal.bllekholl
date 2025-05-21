6d54ff170541db58f68ae54b2a5b824966a9f318
lorikmorina <lorik.morina1@student.uni-pr.edu>
2025-05-21 14:41:53 +0200

    #afficone4

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://*.paddle.com https://afficone.com https://api.afficone.com https://public.profitwell.com https://securevibing.com/api/supacheck/script https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com https://*.securevibing.com; style-src 'self' 'unsafe-inline' https://*.paddle.com; img-src 'self' data: blob: https://lh3.googleusercontent.com; font-src 'self'; connect-src 'self' https://*.supabase.co https://*.paddle.com https://www.google-analytics.com https://metrics.hotjar.io https://challenges.cloudflare.com; frame-src https://challenges.cloudflare.com https://*.paddle.com https://www.youtube.com; "
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://*.paddle.com https://afficone.com https://api.afficone.com https://public.profitwell.com https://securevibing.com/api/supacheck/script https://www.googletagmanager.com https://static.hotjar.com https://script.hotjar.com https://*.securevibing.com; style-src 'self' 'unsafe-inline' https://*.paddle.com; img-src 'self' data: blob: https://lh3.googleusercontent.com; font-src 'self'; connect-src 'self' https://*.supabase.co https://*.paddle.com https://www.google-analytics.com https://metrics.hotjar.io https://challenges.cloudflare.com https://api.afficone.com; frame-src https://challenges.cloudflare.com https://*.paddle.com https://www.youtube.com; "
           },
           {
             key: "X-Frame-Options",
